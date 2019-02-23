763033d0b1459cb9d0b36317009f70f678f7ad45
Roppe Pauline <roppepauline@gmail.com>
2019-02-23 16:22:03 +0100

    mise en ligne tardive

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 
+
+
 // import some polyfill to ensure everything works OK
 import "babel-polyfill"
 
 // import bootstrap's javascript part
+import $ from 'jquery';
 import 'bootstrap';
 
 /*
   Put the JavaScript code you want below.
 */
 
-console.log("Hey look in your browser console. It works!");
+
+$('#myModal').modal('show');
